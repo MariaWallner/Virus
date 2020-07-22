63fff455deae1df9239aca0b09aa7178191ccc8d
wallnerma <wallnerma@cip.ifi.lmu.de>
2020-07-22 12:54:41 +0200

    Anpassungen

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -5,7 +5,10 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuHandler : MonoBehaviour
-{
+{   
+    public Slider slider;
+    public Text easy, normal, hard;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,35 +21,68 @@ public class MenuHandler : MonoBehaviour
         
     }
 
-// wird aufgerufen bei Klick auf den Playbutton
-public void HandlePlay()
-{
-    Debug.Log("Play Button pressed:" + Time.time);
-    SceneManager.LoadScene("Game"); //von einer Scene zu anderen gelangen (von Main zu Game)
-}
+    // wird aufgerufen bei Klick auf den Playbutton
+    public void HandlePlay()
+    {
+        Debug.Log("Play Button pressed:" + Time.time);
+        SceneManager.LoadScene("Game"); //von einer Scene zu anderen gelangen (von Main zu Game)
+    }
 
-public void HandleBack()
-{
-    Debug.Log("Back Button pressed:" + Time.time);
-    SceneManager.LoadScene("Main"); //von einer Scene zu anderen gelangen 
-}
+    public void HandleBack()
+    {
+        Debug.Log("Back Button pressed:" + Time.time);
+        SceneManager.LoadScene("Main"); //von einer Scene zu anderen gelangen 
+    }
 
-public void HandleGameOver()
-{
-    SceneManager.LoadScene("GameOver"); //von einer Scene zu anderen gelangen 
-}
+    public void HandleGameOver()
+    {
+        SceneManager.LoadScene("GameOver"); //von einer Scene zu anderen gelangen 
+    }
 
-// wird aufgerufen bei Klick auf den How To Play Button
-public void HandleHowToPlay()
-{
-    Debug.Log("HowToPay Button pressed:" + Time.time);
-     SceneManager.LoadScene("HowTo"); //von einer Scene zu anderen gelangen 
-}
+    // wird aufgerufen bei Klick auf den How To Play Button
+    public void HandleHowToPlay()
+    {
+        Debug.Log("HowToPay Button pressed:" + Time.time);
+        SceneManager.LoadScene("HowTo"); //von einer Scene zu anderen gelangen 
+    }
 
-// wird aufgerufen bei Klick auf den Quitbutton
-public void HandleQuit()
-{
-    Debug.Log("Quit Button pressed:" + Time.time);
-    Application.Quit();
-}
+    // wird aufgerufen bei Klick auf den Quitbutton
+    public void HandleQuit()
+    {
+        Debug.Log("Quit Button pressed:" + Time.time);
+        Application.Quit();
+    }
+
+    public void HandleLevel()
+    {
+        if (slider != null)
+        {
+            int value = (int) slider.value;
+            Debug.Log(Time.time + "value = " + value);
+
+            switch (value)
+            {
+                case 0:
+                    easy.fontSize = 80;
+                    normal.fontSize = 50;
+                    hard.fontSize = 50;
+                    break;
+                case 1:
+                    easy.fontSize = 50;
+                    normal.fontSize = 80;
+                    hard.fontSize = 50;
+                     break;
+                case 2:
+                    easy.fontSize = 50;
+                    normal.fontSize = 50;
+                    hard.fontSize = 80;
+                     break;
+                default:
+                    easy.fontSize = 80;
+                    normal.fontSize = 50;
+                    hard.fontSize = 50;
+                     break;
+            }
+        }
+    }
 }
