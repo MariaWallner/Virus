da776713a7671113aa07b753c105a97230834590
wallnerma <wallnerma@cip.ifi.lmu.de>
2020-07-24 11:31:23 +0200

    Button Animation

---
+++ Assets/Scripts/TomController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TomController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal") * 13f;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
+    }
+}
