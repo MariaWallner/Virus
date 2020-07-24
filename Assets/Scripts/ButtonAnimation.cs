a3fac599b3a08cdc5797e6e92ef8d6734c3b7f2d
wallnerma <wallnerma@cip.ifi.lmu.de>
2020-07-24 12:40:36 +0200

    ButtenAnimation optimal

---
+++ Assets/Scripts/ButtonAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.EventSystems;
+
+public class ButtonAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+   public void OnPointerEnter(PointerEventData eventData){
+       animator.SetBool("isMouseOver",true);
+   }
+
+    public void OnPointerExit(PointerEventData eventData){
+       animator.SetBool("isMouseOver",false);
+   }
+
+}
